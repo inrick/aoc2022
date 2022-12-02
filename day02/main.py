89420161671ae2d263921fbb5cb744806a5cc35f
Fabian Carlström <inrick@gmail.com>
2022-12-02 19:05:09 +0100

    day02 further substitution

--- day02/main.py
+++ day02/main.py
@@ -5,8 +5,7 @@ input = [(ord(x) - ord('A'), ord(y) - ord('X')) for x, y in input]
 ta, tb = 0, 0
 for a, b in input:
     ta += 3*(a == b) + 6*((a+1)%3 == b) + b + 1
-    b = [(a-1)%3, a, (a+1)%3][b]
-    tb += 3*(a == b) + 6*((a+1)%3 == b) + b + 1
+    tb += 3*b + (a+b-1)%3 + 1
 
 print('a) %d' % ta)
 print('b) %d' % tb)
