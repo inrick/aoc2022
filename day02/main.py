aaf37a9a5edb32b278e48e43a7fa1b9677cc5c4b
Fabian Carlström <inrick@gmail.com>
2022-12-02 18:41:09 +0100

    golf day02
    The previous solution really only depended on the position of each
    string in the 'c' list, we might as well use the numbers directly and
    inline the functions.

--- day02/main.py
+++ day02/main.py
@@ -1,40 +1,12 @@
-c = ['Rock', 'Paper', 'Scissors']
+# golfier solution than first, let (rock, paper, scissors) = (0, 1, 2)
+input = [x.rstrip().split(' ') for x in open('input', 'r')]
+input = [(ord(x) - ord('A'), ord(y) - ord('X')) for x, y in input]
 
-# does a beat b
-def beats(a, b):  return (c.index(a)-1)%3 == c.index(b)
-def toolscore(a): return c.index(a) + 1
-def howtowin(o):  return c[(c.index(o)+1)%3]
-def howtolose(o): return c[(c.index(o)-1)%3]
-
-def roundscore(them, me):
-    if beats(them, me):
-        return 0
-    elif them == me:
-        return 3
-    else:
-        return 6
-
-with open('input', 'r') as f:
-    input = []
-    for m in f:
-        input.append(m.rstrip().split(' '))
-
-total = 0
+ta, tb = 0, 0
 for a, b in input:
-    ta, tb = c[ord(a) - ord('A')], c[ord(b) - ord('X')]
-    total += roundscore(ta, tb) + toolscore(tb)
+    ta += 3*(a == b) + 6*((a+1)%3 == b) + b + 1
+    b = [(a-1)%3, a, (a+1)%3][b]
+    tb += 3*(a == b) + 6*((a+1)%3 == b) + b + 1
 
-print('a) %d' % total)
-
-total = 0
-for a, b in input:
-    ta = c[ord(a) - ord('A')]
-    if b == 'X':
-        tb = howtolose(ta)
-    elif b == 'Y':
-        tb = ta
-    else:
-        tb = howtowin(ta)
-    total += roundscore(ta, tb) + toolscore(tb)
-
-print('b) %d' % total)
+print('a) %d' % ta)
+print('b) %d' % tb)
