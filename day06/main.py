cc77f4bcd50735f84fba6454f2bff741a1b8a4e9
Fabian Carlström <inrick@gmail.com>
2022-12-06 19:46:10 +0100

    day06

--- day06/main.py
+++ day06/main.py
@@ -1,13 +1,7 @@
 input = open('input', 'r').read().rstrip()
 
-a, b = None, None
-for i in range(len(input)):
-    if a is None and len(set(input[i:i+4])) == 4:
-        a = i+4
-    if b is None and len(set(input[i:i+14])) == 14:
-        b = i+14
-    if a and b:
-        break
+def solve(n):
+    return next(i+n for i in range(len(input)) if len(set(input[i:i+n])) == n)
 
-print('a) %d' % a)
-print('b) %d' % b)
+print('a) %d' % solve(4))
+print('b) %d' % solve(14))
