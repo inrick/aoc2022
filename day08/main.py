b4aba8367125528c4f3c84a83cb08d437834ee1e
Fabian Carlström <inrick@gmail.com>
2022-12-09 07:57:59 +0100

    day08 shorten
    Still no satisfying numpy solution though, just shorten the imperative
    one a bit.

--- day08/main.py
+++ day08/main.py
@@ -7,33 +7,25 @@ vis = np.zeros((w-2, h-2), dtype=int)
 for i in range(4):
     vis |= np.rot90((m[1:] > np.maximum.accumulate(m, axis=0)[:-1])[:-1,1:-1], k=4-i)
     m = np.rot90(m)
+
 print('a) %d' % (4*(w-1)+np.sum(vis)))
 
-scenic = np.zeros((w, h), dtype=int)
+def count(height, rows, cols):
+    n = 0
+    for row in rows:
+        for col in cols:
+            n += 1
+            if m[row, col] >= height:
+                return n
+    return n
 
+scenic = np.zeros((w, h), dtype=int)
 for ri in range(1,h-1):
     for cj in range(1,w-1):
-        height = m[ri][cj]
-        up = 0
-        for row in range(ri-1, -1, -1):
-            up += 1
-            if m[row][cj] >= height:
-                break
-        down = 0
-        for row in range(ri+1, h):
-            down += 1
-            if m[row][cj] >= height:
-                break
-        left = 0
-        for col in range(cj-1, -1, -1):
-            left += 1
-            if m[ri][col] >= height:
-                break
-        right = 0
-        for col in range(cj+1, w):
-            right += 1
-            if m[ri][col] >= height:
-                break
+        up    = count(m[ri,cj], range(ri-1, -1, -1), [cj])
+        down  = count(m[ri,cj], range(ri+1, h), [cj])
+        left  = count(m[ri,cj], [ri], range(cj-1, -1, -1))
+        right = count(m[ri,cj], [ri], range(cj+1, w))
         scenic[ri][cj] = up*down*left*right
 
 print('b) %d' % np.max(scenic))
