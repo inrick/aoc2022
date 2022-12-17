8bf81f61af47987c109fbd24b1a4beba5bcc6dd2
Fabian Carlström <inrick@gmail.com>
2022-12-17 16:48:03 +0100

    day15b
    Though a very slow solution: loop through all the rows and look for the
    first position where a beacon could fit.
    
    This uncovered some mistakes in the solution to the first part of the
    problem as well: should not construct impossible intervals, and when
    merging, since they're all on integer coordinates, we should also merge
    intervals with endpoints next to each other.

--- day15/main.py
+++ day15/main.py
@@ -7,24 +7,38 @@ with open('input', 'r') as f:
 def dist(p, q):
     return abs(p[0]-q[0]) + abs(p[1]-q[1])
 
-intervals = []
-for sx, sy, bx, by in input:
-    d = dist((sx,sy), (bx,by))
-    x0 = sx - d + abs(sy - row)
-    x1 = sx + d - abs(sy - row)
-    intervals.append((x0, x1) if x0 < x1 else (x1, x0))
-intervals.sort()
-merged, m = [], None
-while intervals:
-    x = intervals.pop(0)
-    if m is None:
-        m = x
-    elif x[0] <= m[1]:
-        m = [m[0], max(m[1], x[1])]
-    else:
+def rowcover(row):
+    intervals = []
+    for sx, sy, bx, by in input:
+        d = dist((sx,sy), (bx,by))
+        # Since we want abs(sx - x) <= d - abs(sy - row), there is not much use
+        # trying to solve an impossible equation.
+        if d - abs(sy - row) <= 0:
+            continue
+        x0 = sx - d + abs(sy - row)
+        x1 = sx + d - abs(sy - row)
+        intervals.append((x0, x1) if x0 < x1 else (x1, x0))
+    intervals.sort()
+    merged, m = [], None
+    while intervals:
+        x = intervals.pop(0)
+        if m is None:
+            m = x
+        elif x[0] <= m[1]+1:
+            m = [m[0], max(m[1], x[1])]
+        else:
+            merged.append(m)
+            m = x
+    if m:
         merged.append(m)
-        m = x
-if m:
-    merged.append(m)
+    return merged
 
-print('a) %d' % sum(x[1]-x[0] for x in merged))
+def tuning(p): return 4_000_000*p[0]+p[1]
+
+print('a) %d' % sum(x[1]-x[0] for x in rowcover(row)))
+
+for row in range(4_000_001):
+    if len(c := rowcover(row)) > 1:
+        break
+p = (c[0][1]+1, row)
+print('b) %d' % tuning(p))
